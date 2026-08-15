5881e5932faa63f8bff55a6afc54cfe721d5976e
MAYUKA M <mayukamani25@gmail.com>
2026-08-15 20:28:02 +0530

    Time: 3 ms (9.53%) | Memory: 43.4 MB (8.14%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String toLowerCase(String s) {
+        String ans = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= 'A' && c <= 'Z')
+                c = (char)(c + 32);
+            ans += c;
+        }
+        return ans;
+    }
+}
